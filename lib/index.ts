20e5524d6c4c3c5104916fb3e530fa11bfd92fb5
Henry Mueller <henry@handcrafted.dev>
2020-10-31 22:29:30 -0400

    initial

---
+++ lib/index.ts
@@ -0,0 +1,57 @@
+import * as t from "io-ts";
+import {
+  chain,
+  chainEitherK,
+  left,
+  right,
+  TaskEither,
+  tryCatch,
+} from "fp-ts/lib/TaskEither";
+import { flow, pipe } from "fp-ts/lib/function";
+import {
+  mapLeft,
+  toError,
+} from "fp-ts/lib/Either";
+import { failure } from "io-ts/lib/PathReporter";
+
+interface HTTPError extends Error {
+  status: number;
+}
+type FetchError = Error | HTTPError;
+
+const fetchUrlThunk = (input: RequestInfo, init?: RequestInit) => () => {
+  return fetch(input, init).then(async (response) => {
+    const json = await response.json().catch((_) => null);
+    return { response, json };
+  });
+};
+
+const parseFetchResponse = ({
+  response,
+  json,
+}: {
+  response: Response;
+  json: any;
+}): TaskEither<FetchError, any> => {
+  return response.ok
+    ? right(json)
+    : left({
+        name: "HTTPError",
+        status: response.status,
+        message: `${response.status}`,
+      });
+};
+
+const validationErrorsToError = (errors: t.Errors): Error =>
+  new Error(failure(errors).join("/n"));
+
+export const fetchAndDecode = <R>(responseType: t.Type<R>) => (
+  input: RequestInfo,
+  init?: RequestInit
+): TaskEither<FetchError, R> => {
+  return pipe(
+    tryCatch(fetchUrlThunk(input, init), toError),
+    chain(parseFetchResponse),
+    chainEitherK(flow(responseType.decode, mapLeft(validationErrorsToError)))
+  );
+};
---
+++ test/test.ts
@@ -0,0 +1,55 @@
+import tap from "tap";
+import fetch from "node-fetch";
+import * as t from "io-ts";
+import { fetchAndDecode } from "../lib";
+import { isLeft, isRight, mapLeft } from "fp-ts/lib/Either";
+
+globalThis.fetch = fetch;
+
+const Todo = t.type(
+  {
+    userId: t.number,
+    id: t.number,
+    title: t.string,
+    completed: t.boolean,
+  },
+  "Todo"
+);
+
+type Todo = t.TypeOf<typeof Todo>;
+
+tap.test("real fetch works", async (t) => {
+  const fetcher = fetchAndDecode(Todo)(
+    "https://jsonplaceholder.typicode.com/todos/1"
+  );
+  const result = await fetcher();
+  t.true(isRight(result));
+});
+
+const TodoWithError = t.type(
+  {
+    userId: t.number,
+    id: t.string,
+    title: t.string,
+    completed: t.boolean,
+  },
+  "Todo"
+);
+
+type TodoWithError = t.TypeOf<typeof TodoWithError>;
+
+tap.test("fetch decode returns error", async (t) => {
+  const fetcher = fetchAndDecode(TodoWithError)(
+    "https://jsonplaceholder.typicode.com/todos/1"
+  );
+  const result = await fetcher();
+  t.true(isLeft(result));
+});
+
+tap.test("404 response returns well formed error", async (t) => {
+  const fetcher = fetchAndDecode(Todo)(
+    "https://httpstat.us/404"
+  );
+  const result = await fetcher();
+  t.true(isLeft(result));
+});
